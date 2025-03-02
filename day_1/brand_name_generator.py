ca0324ddcaa51d65da645bc443298410428357af
Siva Karthik Narisetty <karthik.siva555@gmail.com>
2025-03-02 16:30:47 -0500

    Rename project to python_practice, complete day 1 learning and assignment

---
+++ day_1/brand_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Brand Name Generator.")
+city= input("What is the city name you grew up in?\n")
+pet = input("What is your pet's name?\n")
+print(f"Your brand name could be {city} {pet}")
---
+++ day_1/day_1.py
@@ -0,0 +1,34 @@
+# print a message
+print("hello Python learning")
+
+# new line character \n
+print("hello python! \n Have a great day")
+
+# use plus to concatenate strings
+name = "siva" + "karthik"
+print(name)
+
+# input function and fstring
+designation = input("please enter your designation: ")
+print(f"Designation: {designation}")
+
+# len() to print the length of a string
+name = "sivakarthik"
+print(len(name))
+
+# len() to print the length of an array/list
+names = ["siva", "karthik", "vaishu", "aara"]
+print(len(names))
+
+# variables in Python follow snake case
+long_name = "Siva Karthik Narisetty"
+print(long_name)
+
+# implicit continuation in string
+name = ("siva"
+        "karthik")
+print(f"{name} is implicit continuation string")
+
+
+
+
