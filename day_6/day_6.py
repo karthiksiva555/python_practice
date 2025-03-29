abdfd148a1826aa00f62d58d2e28b688dfd406d6
Siva Karthik Narisetty <karthik.siva555@gmail.com>
2025-03-29 14:45:03 -0400

    Day 6 - practice functions and while loops

---
+++ day_6/day_6.py
@@ -0,0 +1,20 @@
+# Code blocks, functions, and while loops
+
+# functions
+def my_function():
+    print("Hello")
+    print("My first function")
+
+my_function()
+
+# indentation with only two spaces
+def indentation_test():
+  print("no error")
+
+indentation_test()
+
+# while loops
+count = 5
+while count > 0:
+    print(count)
+    count-=1
