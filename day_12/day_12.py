f0d9dac46ff7df73981133030cd8cdb577ba6d2f
Siva Karthik Narisetty <karthik.siva555@gmail.com>
2025-05-15 21:18:08 -0400

    Day 12 - practice scope and is_prime

---
+++ day_12/day_12.py
@@ -0,0 +1,52 @@
+# local vs global scope
+import math
+
+cars_sold = 10 # global variable
+
+def sell_car():
+    cars_sold = 11 # local variable overshadows the global variable
+    print(cars_sold) # 11
+
+sell_car()
+print(cars_sold) # 10
+
+def start_game():
+    def get_players_ready(): # An inner function local to start_game()
+        print("Players are ready!")
+
+    get_players_ready()
+    print("game started")
+
+
+start_game()
+# get_players_ready() can't be called as its only local to start_game()
+
+sold_price = 15.50
+
+def calculate_discount():
+    if sold_price > 10:
+        discount_percent = 5
+
+    print(f"You get {discount_percent}% discount")
+
+calculate_discount()
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    if num <= 3:
+        return True
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+
+    sqrt_num = int(math.sqrt(num)) + 1
+    print(f"looping till {sqrt_num}")
+    for i in range(5, sqrt_num, 6):
+        print(f"checking {i} or {i+2} is a divisor for {num}")
+        if n % i == 0 or n % (i+2) == 0:
+            return False
+
+    return True
+
+n = 523
+print(f"{n} is a prime number: {is_prime(n)}")
