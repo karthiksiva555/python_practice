311ea66d59b00dfa5870a68777682e11f44099eb
Siva Karthik Narisetty <karthik.siva555@gmail.com>
2025-08-01 06:36:10 -0400

    refactor: day 22 - change Paddle to inherit from Turtle

--- days_21_to_40/day_22/paddle.py
+++ days_21_to_40/day_22/paddle.py
@@ -6,44 +6,43 @@ STEP = 20
 LEFT_PADDLE_POSITION = (-380, 0)
 RIGHT_PADDLE_POSITION = (370, 0)
 
-def get_paddle(position):
-    paddle = Turtle("square")
-    paddle.color("white")
-    paddle.shapesize(stretch_wid=5, stretch_len=1)
-    paddle.penup()
-    paddle.goto(position)
 
-    return paddle
-
-class Paddle:
+class Paddle(Turtle):
 
     def __init__(self, left_or_right):
+        super().__init__()
         if left_or_right == "left":
-            self.paddle = get_paddle(LEFT_PADDLE_POSITION)
-        elif left_or_right == "right":
-            self.paddle = get_paddle(RIGHT_PADDLE_POSITION)
+            position = LEFT_PADDLE_POSITION
+        else:
+            position = RIGHT_PADDLE_POSITION
+
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
 
     def move_up(self):
         if not self.can_paddle_move_up():
             return
 
-        new_position = (self.paddle.xcor(), self.paddle.ycor() + STEP)
-        self.paddle.setposition(new_position)
+        new_position = (self.xcor(), self.ycor() + STEP)
+        self.setposition(new_position)
 
     def move_down(self):
         if not self.can_paddle_move_down():
             return
 
-        new_position = (self.paddle.xcor(), self.paddle.ycor() - STEP)
-        self.paddle.setposition(new_position)
+        new_position = (self.xcor(), self.ycor() - STEP)
+        self.setposition(new_position)
 
     def can_paddle_move_up(self):
-        if self.paddle.ycor() < TOP_RANGE:
+        if self.ycor() < TOP_RANGE:
             return True
         return False
 
     def can_paddle_move_down(self):
-        if self.paddle.ycor() >= BOTTOM_RANGE:
+        if self.ycor() >= BOTTOM_RANGE:
             return True
         return False
 
