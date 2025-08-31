3c883ce08278fe9c4138c61c4baa8c02d85c42fa
Siva Karthik Narisetty <karthik.siva555@gmail.com>
2025-08-31 16:32:42 -0400

    Day 27 - Practice pack, place and grid

---
+++ days_21_to_40/day_27/pack_place_grid.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+window = Tk()
+window.title("Pack Place Grid")
+window.minsize(height=400, width=400)
+
+label = Label(text="Place Me")
+# label.pack(side="left")
+# label.place(x=0, y=0)
+label.grid(row=0, column=0)
+
+button = Button(text="Submit")
+# button.pack(side="right")
+# button.place(x=100, y=200)
+button.grid(row=2, column=1)
+
+text_box = Entry()
+# text_box.pack(side="left")
+# text_box.place(x=300, y=250)
+text_box.grid(row=1, column=2)
+
+
+window.mainloop()
